1487073a360dcf96393bb5664a1ec2fa2e827be0
Bua T <bua19_1@hotmail.com>
2016-10-11 23:38:45 +0000

    Adding skeleton models and dbtables for Tournament and User. Simple action handler for tournament's add/edit/delete also included.

---
+++ golfly/app/controllers/tournaments_controller.rb
@@ -0,0 +1,33 @@
+class TournamentsController < ApplicationController
+#simple handling methods for testing purpose. 
+  def index
+    @tournaments = Tournament.all
+  end
+  def new
+  end
+  def show
+    @tournament = Tournament.find(params[:id])
+  end	
+  def edit
+    @tournament = Tournament.find(params[:id])
+  end
+  def create
+    @tournament = Tournament.new(tnm_params)
+    @tournament.save
+    redirect_to @tournament
+  end
+  def update
+    @tournament = Tournament.find(params[:id])  
+    @tournament.update(tnm_params)
+  end 
+  def destroy
+    @tournament = Tournament.find(params[:id])
+    @tournament.destroy
+ 
+    redirect_to tournaments_path
+  end
+  private
+    def tnm_params
+      params.require(:tournament).permit(:title, :location, :start, :end, :description)
+    end
+end
---
+++ golfly/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+#to be defined depending on how users are created
+  def show
+  end
+  def new
+  end
+  def edit
+  end
+  def create
+  end
+  def update
+  end
+  def delete
+  end
+
+end
