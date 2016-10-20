18cc6b8be7c01c4df55134a51769ad8ae59982a8
heindrik <heindrikbernabe@gmail.com>
2016-10-20 03:23:44 -0400

    -Initial Commit -Started on home page -Added Login and Sign up buttons on navbar -Added route and page for about -Added Tournament listings on home page

--- golfly/app/controllers/home_controller.rb
+++ golfly/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@tournaments = Tournament.all
+  end
+  def about
   end
 end
--- golfly/config/routes.rb
+++ golfly/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root 'home#index'
+  get 'about', to: 'home#about' 
 
   resources :users
   resources :tournaments
