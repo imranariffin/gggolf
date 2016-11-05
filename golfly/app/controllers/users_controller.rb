f1602d14d8fa4b4c28a0adcdf6d64a2824640913
steve148 <lennox.stevenson@mail.utoronto.ca>
2016-11-05 14:57:24 -0400

    starting to clean up profile page
    - added bio text column to users
    - worked on edit page
    - worked on show page

--- golfly/app/controllers/users_controller.rb
+++ golfly/app/controllers/users_controller.rb
@@ -2,11 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def new
-    @user = User.new()
+    @user = User.new
   end
+
   def show
-    @user = User.find(params[:id])
+    @user = User.find params[:id]
 
     # if @user.image_path
     #   @image_path = @user.image_path
@@ -16,30 +18,32 @@ class UsersController < ApplicationController
     @image_path = "http://www.eecg.toronto.edu/~stumm/stumm.jpg"
 
     # show tournaments
-    @players = @user.players.all()
-    @admins = @user.admins.all()
-    @sponsors = @user.sponsors.all()
+    @players = @user.players
+    @admins = @user.admins
+    @sponsors = @user.sponsors
   end 
+
   def edit
-    @user = User.find(params[:id])
+    @user = User.find params[:id]
   end
+
   def create
-    @user = User.new(usr_params)
-    @user.save
+    @user = User.create usr_params
     redirect_to @user
   end
+
   def update
-    @user = User.find(params[:id])  
-    @user.update(usr_params)
+    @user = User.update params[:id], usr_params
 
     redirect_to user_path
   end 
+
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+    @user = User.find(params[:id]).destroy
  
     redirect_to users_path
   end
+
   private
     def usr_params
       params.require(:user).permit(:fname, :lname, :dob, :email, :phone)
--- golfly/db/schema.rb
+++ golfly/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018204209) do
+ActiveRecord::Schema.define(version: 20161105184924) do
 
   create_table "admins", force: :cascade do |t|
     t.integer  "user_id"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20161018204209) do
     t.string   "addr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "bio"
   end
 
 end
