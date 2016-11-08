1af9801e3ab6a4b7f5e3fdfc7d25bcf6adb08f7f
imranariffin <imran.ariffin@mail.utoronto.ca>
2016-11-08 16:59:49 -0500

    join tournament

--- golfly/app/controllers/tournaments_controller.rb
+++ golfly/app/controllers/tournaments_controller.rb
@@ -8,7 +8,18 @@ class TournamentsController < ApplicationController
   end
   def show
     @tournament = Tournament.find(params[:id])
-    @users = User.all
+    @players = @tournament.players()
+
+    # get all users playing in this tournament
+    @users = @players.map do |p|
+      p.user
+    end
+
+    # available spot progress bar
+    player_limit = @tournament.player_limit.to_f
+    player_joined = @players.length.to_f
+    @available_spot = ((player_limit - player_joined)/player_limit * 100).to_i
+    # @available_spot = 10
   end	
   def edit
     @tournament = Tournament.find(params[:id])
@@ -29,6 +40,22 @@ class TournamentsController < ApplicationController
  
     redirect_to tournaments_path
   end
+
+  # current user joins a tournament as player
+  def join
+    @user = current_user
+    @tournament = Tournament.find(params[:id])
+
+    # update tournament
+    @user_team = @tournament.teams.new()
+    if @user_team.save()
+      @user_player = @user.players.new(team_id: @user_team[:id])
+      if @user_player.save()
+        redirect_to tournament_path
+      end
+    end
+  end
+
   private
     def tnm_params
       params.require(:tournament).permit(:title, :location, :start, :end, :description)
