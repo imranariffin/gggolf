f1f658c8c3d896bdd82665224722a9df556344fb
alexcheng068 <alexcheng068@gmail.com>
2016-11-30 22:19:57 +0000

    minor fix

--- db/schema.rb
+++ db/schema.rb
@@ -26,11 +26,6 @@ ActiveRecord::Schema.define(version: 20161129054552) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "emails", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "players", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "team_id"
