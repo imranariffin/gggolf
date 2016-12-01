634d2934dfab442104eaa1d6b11cd509bd408fbb
heindrik <heindrikbernabe@gmail.com>
2016-11-30 19:28:10 -0500

    finished sponsor registration

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -13,11 +13,12 @@ class SponsorsController < ApplicationController
 		@tournament = Tournament.find params[:tournament_id]
 		@sponsor.user_id = current_user.id
       	@sponsor.tournament_id = @tournament.id
+      	@sponsor.sponsor_opt_id = params[:sponsor_selected]
 
       	if @sponsor.save
   	  		redirect_to tournament_path(@tournament)
   		else
-  			flash[:error] = "Our bad, something broke"
+  			flash[:error] = "Oops, something broke!"
   	  		redirect_to tounament_sponsor_path(@tournament)
   		end
 	end
